547ad7cd930d248d148479e55603eaacd5979e82
Yashita-Namdeo <yashita.namdeo2000@gmail.com>
2021-07-27 17:23:56 +0530

    add comment in python

---
+++ fork-python/Module1/9_comment_in_python.py
@@ -0,0 +1,28 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+def print_func():
+    #print ("Geeks Classes")
+    print ("Geeks For Geeks")
+    '''print ("Geeks Quiz")
+    print ("Geeks")
+    print ("Geeks For")'''
+
+#{ 
+#Driver Code Starts.
+
+#Python code to use comment
+
+# Driver Code
+def main():
+    
+    print_func()
+        
+
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
