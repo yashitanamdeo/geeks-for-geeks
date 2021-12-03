093ba3142c2c818ea15ad020dec3f4a10c499d5b
Yashita Namdeo <yashita.namdeo2000@gmail.com>
2021-12-03 22:39:16 +0530

    Update 3_check_the_status.py

--- fork-python/Module2/3_check_the_status.py
+++ fork-python/Module2/3_check_the_status.py
@@ -8,15 +8,16 @@
 # Function to check value and 
 # return accordingly
 def check_status(a, b, flag):
-    if a == 1 and b == 1:
-        return False
-    elif flag == True:
+    if flag:
         if a < 0 and b < 0:
             return True
-    elif a > 0 or b > 0:
-        return True
+        else:
+            return False
     else:
-        return False
+        if (a > 0 and b < 0) or (a < 0 and b > 0):
+            return True
+        else:
+            return False
 
 #{ 
 #Driver Code Starts.
