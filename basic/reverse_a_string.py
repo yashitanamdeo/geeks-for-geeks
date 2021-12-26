2391ef38d24850ff6d13e39a18e49cbe8fcf3057
yashitanamdeo <yashita.namdeo2000@gmail.com>
2021-12-26 19:39:12 +0530

    add Reverse a String

--- basic/reverse_a_string.py
+++ basic/reverse_a_string.py
@@ -1,1 +1,20 @@
 # Problem Statement: https://practice.geeksforgeeks.org/problems/reverse-a-string/
+
+#User function Template for python3
+
+def reverseWord(s):
+    #your code here
+    return s[::-1]
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    t = int(input())
+    while(t>0):
+        s = input()
+        print(reverseWord(s))
+        t = t-1
+
+# } Driver Code Ends
