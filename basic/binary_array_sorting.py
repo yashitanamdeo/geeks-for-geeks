ade53d6d4505e251a90b07e05b43d2a81de20bd2
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-06 01:21:50 +0530

    update Binary Array Sorting

--- basic/binary_array_sorting.py
+++ basic/binary_array_sorting.py
@@ -9,7 +9,17 @@ class Solution:
         #Your code here
         #No need to print the array
         return A.sort() # simple inbuilt method to sort array/list
-
+'''
+    def binSort(self, A, N): 
+            #Your code here
+            #No need to print the array
+            j = 0
+            for i in range(N):
+                if A[i]==0: # agar 1 hai toh hum i ki value badhne denge jab tak i 0 nhi hojata
+                    A[i],A[j] = A[j],A[i] # jab i 0 hojayega tab hum A[j] ki value se swap/replace kardenge because woh hamesha 1 hi rehega 
+                    j += 1 # jab hum replace karenge tab hi j ko agey badhayenge
+            return A
+'''
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
