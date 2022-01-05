947aed413903976c73fccd83afff0afe93604880
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-05 23:19:29 +0530

    add Binary Array Sorting

--- basic/binary_array_sorting.py
+++ basic/binary_array_sorting.py
@@ -1,1 +1,37 @@
 # Problem Statement: https://practice.geeksforgeeks.org/problems/binary-array-sorting-1587115620/1#
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to sort the binary array.
+    def binSort(self, A, N): 
+        #Your code here
+        #No need to print the array
+        return A.sort() # simple inbuilt method to sort array/list
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+def main():
+        T=int(input())
+        while(T>0):
+            N=int(input())
+            A=list(map(int,input().split()))
+            obj = Solution()
+            obj.binSort(A,N)
+            
+            for i in A:
+                print(i,end=" ")
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
