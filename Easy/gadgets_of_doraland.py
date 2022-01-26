d038432a8b810be7f56b8df49a7a606ffc6ef8c8
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-26 23:44:29 +0530

    add Gadgets of Doraland

--- Easy/gadgets_of_doraland.py
+++ Easy/gadgets_of_doraland.py
@@ -1,1 +1,41 @@
 # Problem Statement: https://practice.geeksforgeeks.org/problems/bbd15e2da95880979ce65acc7360e2c5681664e65520/1
+
+#User function Template for python3
+
+class Solution:
+    def TopK(self, array, k):
+        # code here
+        d={}
+        x=[]
+        out=[]
+        for i in array:
+            if i in d:
+                d[i] += 1
+            else:
+                d[i] = 1
+        for i in d:
+            x.append([i,d[i]])
+        x.sort(reverse=True,key=lambda x:(x[1],x[0]))
+        for i in range(k):
+            out.append(x[i][0])
+        return out
+
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        array = [int(x) for x in input().strip().split()]
+        k = int(input())
+        obj = Solution()
+        res = obj.TopK(array, k)
+        for each in res:
+            print(each, end=' ')
+        print()
+
+# } Driver Code Ends
