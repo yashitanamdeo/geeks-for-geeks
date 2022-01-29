c0699946ccb8a6739a1e0ed79e97e2a5ff18e96f
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-29 23:30:10 +0530

    add Reverse words in a given string

--- Easy/reverse_words_in_a_given_string.py
+++ Easy/reverse_words_in_a_given_string.py
@@ -1,1 +1,27 @@
 # Problem Statement: https://practice.geeksforgeeks.org/problems/reverse-words-in-a-given-string5459/1#
+
+
+# User function Template for python3
+
+class Solution:
+    
+    #Function to reverse words in a given string.
+    def reverseWords(self,S):
+        # code here 
+        ans = ''
+        S = S.split(".")
+        ans = S[::-1]
+        return ".".join(ans)
+            
+#{ 
+#  Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        s = str(input())
+        obj = Solution()
+        print(obj.reverseWords(s))
+
+# } Driver Code Ends
