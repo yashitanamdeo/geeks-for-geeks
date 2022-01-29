63b8cc7c44ad3c0041483167a8c35c0ba46e6bb1
yashitanamdeo <yashita.namdeo2000@gmail.com>
2022-01-29 23:32:54 +0530

    update Reverse words in a given string

--- Easy/reverse_words_in_a_given_string.py
+++ Easy/reverse_words_in_a_given_string.py
@@ -12,6 +12,19 @@ class Solution:
         S = S.split(".")
         ans = S[::-1]
         return ".".join(ans)
+        #return '.'.join(S.split('.')[::-1])
+
+    '''
+    def reverseWords(self,S):
+        list1=S.split('.')
+        ans_str=''
+        for i in range(len(list1)-1,-1,-1):
+            if i >0:
+                ans_str=ans_str+list1[i]+'.'
+            elif i==0:
+                ans_str=ans_str+list1[i]
+        return ans_str
+    '''
             
 #{ 
 #  Driver Code Starts
