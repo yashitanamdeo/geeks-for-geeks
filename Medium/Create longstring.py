8114a41cec96ddb3c1daa6f42ba13d8b06a3d093
Yashita Namdeo <yashita.namdeo2000@gmail.com>
2023-01-05 19:59:58 +0530

    Update Create longstring.py

--- Medium/Create longstring.py
+++ Medium/Create longstring.py
@@ -1,25 +1,34 @@
+# Problem Statement: https://practice.geeksforgeeks.org/problems/8d157f11af5416087251513cfc38ffc4d23be308/1
+
+#User function Template for python3
+
+
 class Solution():
 
     def longestString(self, arr, n):
 
-         #your code goes here
-
+        #your code goes here
         arr.sort()
-
         max=""
-
         dict={}
-
         dict[""]=1
-
         for i in range(n):
-
             if arr[i][:-1] in dict:
-
                 dict[arr[i]]=1
-
                 if len(max)<len(arr[i]):
-
                     max=arr[i]
 
         return max
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n = int(input())
+        arr = [i for i in input().split()]
+        print(Solution().longestString(arr,n))
+# } Driver Code Ends
