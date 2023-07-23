696bc2bdf75a0d7c63b2d9cd5cbeeeb042f381b6
yashitanamdeo <yashita.namdeo2000@gmail.com>
2023-07-23 23:48:22 +0530

    add Given a linked list of 0s, 1s and 2s, sort it

---
+++ Easy/given_a_linked_list_of_0s_1s_and_2s_sort_it.py
@@ -0,0 +1,107 @@
+# Problem Statement: https://practice.geeksforgeeks.org/problems/given-a-linked-list-of-0s-1s-and-2s-sort-it/1
+
+# User function Template for python3
+'''
+	Your task is to segregate the list of 
+	0s,1s and 2s.
+	
+	Function Arguments: head of the original list.
+	Return Type: head of the new list formed.
+
+	{
+		# Node Class
+		class Node:
+		    def __init__(self, data):   # data -> value stored in node
+		        self.data = data
+		        self.next = None
+	}
+
+'''
+import sys
+import io
+import atexit
+
+
+class Solution:
+    # Function to sort a linked list of 0s, 1s and 2s.
+    def segregate(self, head):
+        c = [0]*3
+        curr = head
+        while curr:
+            c[curr.data] += 1
+            curr = curr.next
+        i = 0
+        curr = head
+        while curr:
+            if c[i]:
+                curr.data = i
+                c[i] -= 1
+                curr = curr.next
+            else:
+                i += 1
+        return head
+
+
+# {
+ # Driver Code Starts
+# Initial Template for Python 3
+# Contributed by : Nagendra Jha
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+# Node Class
+
+
+class Node:
+    def __init__(self, data):   # data -> value stored in node
+        self.data = data
+        self.next = None
+
+# Linked List Class
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # creates a new node with given value and appends it at the end of the linked list
+    def append(self, new_value):
+        new_node = Node(new_value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            return
+        self.tail.next = new_node
+        self.tail = new_node
+
+# prints the elements of linked list starting with head
+
+
+def printList(head):
+    if head is None:
+        print(' ')
+        return
+    curr_node = head
+    while curr_node:
+        print(curr_node.data, end=" ")
+        curr_node = curr_node.next
+    print()
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for cases in range(t):
+        n = int(input())
+        a = LinkedList()  # create a new linked list 'a'.
+        nodes_a = list(map(int, input().strip().split()))
+        for x in nodes_a:
+            a.append(x)  # add to the end of the list
+        printList(Solution().segregate(a.head))
+# } Driver Code Ends
