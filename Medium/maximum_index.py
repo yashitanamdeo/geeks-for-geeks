827d730a784384f738964fbf83c85d84332063b6
Yashita Namdeo <yashita.namdeo2000@gmail.com>
2024-03-05 23:47:39 +0530

    Add Solution 2 for Maximum Index

--- Medium/maximum_index.py
+++ Medium/maximum_index.py
@@ -20,6 +20,23 @@ class Solution:
                 diff=max(diff,far-curr)
             
         return diff
+    
+    # Solution 2
+    ''' 
+    def maxIndexDiff(self,a, n): 
+        l_min,r_max=[a[0]]*n,[a[-1]]*n
+        for i in range(1,n):
+            l_min[i]=min(l_min[i-1],a[i])
+            r_max[-i-1]=max(r_max[-i],a[-i-1])
+        l,r,ans=0,0,0
+        while l<n and r<n:
+            if l_min[l]<=r_max[r]:
+                ans=max(ans,r-l)
+                r+=1
+            else:
+                l+=1
+        return ans
+    '''
 
 
 #{ 
