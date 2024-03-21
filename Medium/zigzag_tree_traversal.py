85a04368f74b29ae0a2a25cdc5bb13810bd14b4f
Yashita Namdeo <yashita.namdeo2000@gmail.com>
2024-03-21 23:11:11 +0530

    add ZigZag Tree Traversal

---
+++ Medium/zigzag_tree_traversal.py
@@ -0,0 +1,123 @@
+# Problem Statement: https://www.geeksforgeeks.org/problems/zigzag-tree-traversal/1
+
+#User function Template for python3
+
+'''
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+
+from collections import deque
+class Solution:
+    def zigZagTraversal(self, root):
+        ans=[]
+        d=deque()
+        d.append(root)
+        ltr=True
+        while d:
+            n=len(d)
+            for _ in range(n):
+                if ltr:
+                    item=d.popleft()
+                else:
+                    item=d.pop()
+                ans.append(item.data)
+                if ltr:
+                    if item.left:
+                        d.append(item.left)
+                    if item.right:
+                        d.append(item.right)
+                else:
+                    if item.right:
+                        d.appendleft(item.right)
+                    if item.left:
+                        d.appendleft(item.left)
+            ltr= not ltr
+        return ans
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+
+from collections import defaultdict
+from collections import deque
+
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+        
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob = Solution()
+        res = ob.zigZagTraversal(root)
+        for i in range (len (res)):
+            print (res[i], end = " ")
+        print()
+# } Driver Code Ends
