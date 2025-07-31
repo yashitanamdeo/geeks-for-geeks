4996b3fbbd0d1fb433715709489427e4ac6c3d2d
Yashita <yashita.namdeo2000@gmail.com>
2025-07-31 23:48:39 +0530

    Add Solution2 for 'Powerful Integer'

--- medium/powerful_integer/solution.py
+++ medium/powerful_integer/solution.py
@@ -24,6 +24,24 @@ class Solution:
         return powerfull_integer
 
 
+class Solution2:
+    def powerfulInteger(self, intervals, k):
+        freq={}
+        for item in intervals:
+            freq[item[0]]=freq.get(item[0],0)+1
+            freq[item[1]+1]=freq.get(item[1]+1,0)-1
+        freq=list(freq.items())
+        freq.sort()
+        ans=-1
+        count=0
+        for item in freq:
+            if count>=k:
+                ans=item[0]-1
+            count+=item[1]
+        return ans
+
+
+
 #{
  # Driver Code Starts
 class IntMatrix:
