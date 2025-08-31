f80806a6f12aa5f9ffbc45efe7d2ed91acbfbf74
Yashita <yashita.namdeo2000@gmail.com>
2025-08-31 23:41:50 +0530

    Added Solution for 'Container With Most Water'

--- medium/container_with_most_water/solution.py
+++ medium/container_with_most_water/solution.py
@@ -33,3 +33,20 @@ for _ in range(0, int(input())):
 
 
 # } Driver Code Ends
+
+
+
+class Solution2:
+    def maxWater(self, arr):
+        # code here
+        left, right, max_area = 0, len(arr) - 1, 0
+        while left < right:
+            if arr[left] < arr[right]:
+                area = arr[left] * (right - left)
+                left += 1
+            else:
+                area = arr[right] * (right - left)
+                right -= 1
+            if area > max_area:
+                max_area = area
+        return max_area
