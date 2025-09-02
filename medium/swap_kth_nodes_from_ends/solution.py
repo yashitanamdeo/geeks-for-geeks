8364ab72f41bfd515a1b10fea78ef69b028edd2a
Yashita <yashita.namdeo2000@gmail.com>
2025-09-02 23:42:46 +0530

    Added Solution2 for 'Swap Kth nodes from ends'

--- medium/swap_kth_nodes_from_ends/solution.py
+++ medium/swap_kth_nodes_from_ends/solution.py
@@ -55,6 +55,29 @@ def swapkthnode(head, num, k):
     
     return head
 
+
+'''
+class Node:
+    def __init__(self, x):
+        self.data = x
+        self.next = None
+'''
+class Solution2:
+    def swapKth(self, head, k):
+        curr=head
+        for _ in range(k-1):
+            curr=curr.next
+            if curr is None:
+                return head
+        kth=curr
+        end_kth=head
+        while curr.next:
+            curr=curr.next
+            end_kth=end_kth.next
+        kth.data,end_kth.data=end_kth.data,kth.data
+        return head
+
+
 #{ 
 #  Driver Code Starts
 #Initial Template for Python 3
