0f03d5b1b3fea834f425f5cf66d00ec5032a398a
Yashita <yashita.namdeo2000@gmail.com>
2025-09-05 23:28:03 +0530

    Added Solution2 for 'Sort a linked list of 0s, 1s and 2s'

--- medium/sort_a_linked_list_of_0s_1s_and_2s/solution.py
+++ medium/sort_a_linked_list_of_0s_1s_and_2s/solution.py
@@ -42,6 +42,36 @@ class Solution:
         return head
 
 
+class Solution2:
+    def segregate(self, head):
+        zero,one,two=Node(-1),Node(-1),Node(-1)
+        zh,oh,th=zero,one,two
+        curr=head
+        while curr:
+            if curr.data==0:
+                zero.next=curr
+                zero=zero.next
+            elif curr.data==1:
+                one.next=curr
+                one=one.next
+            else:
+                two.next=curr
+                two=two.next
+            curr=curr.next
+        zero.next,one.next,two.next=None,None,None
+        head=Node(-1)
+        tail=head
+        if zh.next:
+            tail.next=zh.next
+            tail=zero
+        if oh.next:
+            tail.next=oh.next
+            tail=one
+        if th.next:
+            tail.next=th.next
+            tail=two
+        return head.next
+
 # {
  # Driver Code Starts
 # Initial Template for Python 3
