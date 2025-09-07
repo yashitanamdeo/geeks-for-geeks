ec673040ae6492a0250ea76507f37d491dc852d1
Yashita <yashita.namdeo2000@gmail.com>
2025-09-07 23:09:14 +0530

    Added Solution2 for 'Merge K sorted linked lists'

--- medium/merge_k_sorted_linked_lists/solution.py
+++ medium/merge_k_sorted_linked_lists/solution.py
@@ -36,6 +36,23 @@ class Solution:
             str.next=node
             str = node
         return head
+    
+
+class Solution2:
+    def mergeKLists(self, arr):
+        import heapq
+        hp=[(nd.data,ix,) for ix,nd in enumerate(arr)]
+        heapq.heapify(hp)
+        dum=Node(-1)
+        cur=dum
+        while hp:
+            _,ix=heapq.heappop(hp)
+            cur.next=arr[ix]
+            cur=cur.next
+            arr[ix]=arr[ix].next
+            if arr[ix]:
+                heapq.heappush(hp,(arr[ix].data,ix,))
+        return dum.next
 
 #{ 
 #  Driver Code Starts
