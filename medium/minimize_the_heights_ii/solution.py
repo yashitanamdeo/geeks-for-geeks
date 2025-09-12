3c13aaac73fc35889380957959bdc1c3718c2c46
Yashita <yashita.namdeo2000@gmail.com>
2025-09-12 23:03:52 +0530

    Added Solution2 for 'Minimize the Heights II'

--- medium/minimize_the_heights_ii/solution.py
+++ medium/minimize_the_heights_ii/solution.py
@@ -28,6 +28,21 @@ class Solution:
         return result
 
 
+class Solution2:
+    def getMinDiff(self, arr, k):
+        # code here
+        arr.sort()
+        res = arr[-1] - arr[0]
+        small = arr[0] + k
+        big = arr[-1] - k
+        for i in range(1, len(arr)):
+            if(arr[i] - k < 0):
+                continue
+            mini = min(small, arr[i] - k)
+            maxi = max(big, arr[i-1] + k)
+            res = min(res, maxi - mini)
+        return res
+
 # {
  # Driver Code Starts
 # Initial Template for Python 3
