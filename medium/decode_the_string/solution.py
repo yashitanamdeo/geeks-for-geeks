48db6c4eb0ea2afa9a0ec9dd26396f998b1a63fb
Yashita <yashita.namdeo2000@gmail.com>
2025-09-17 23:30:15 +0530

    Added Solution2 for 'Decode the string'

--- medium/decode_the_string/solution.py
+++ medium/decode_the_string/solution.py
@@ -38,6 +38,27 @@ class Solution:
         return finalString
 
 
+class Solution2:
+    def decodedString(self, s):
+        # code here
+        stack = []
+        for i in s:
+            if i == ']':
+                expand = ''
+                while stack[-1] != '[':
+                    expand = stack.pop() + expand
+                stack.pop()
+
+                mul = ''
+                while stack and stack[-1].isdigit():
+                    mul = stack.pop() + mul
+                
+                stack.append(int(mul) * expand)
+                continue
+            
+            stack.append(i)
+            
+        return ''.join(stack)
 #{
  # Driver Code Starts
 #Initial Template for Python 3
